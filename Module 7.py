0445b4574463c7486642a767d8f1560ac8c2cb4d
jjackson51 <48562392+jjackson51@users.noreply.github.com>
2019-03-21 12:48:24 -0500

    Add files via upload

---
+++ Module 7.py
@@ -0,0 +1,46 @@
+#Jamaree jackson
+# 3/7/19
+
+# Takes a list and checks for a value
+
+def listcheck(n):
+     for x in n:
+         if x == 5:
+             print("True")
+
+numberList = [1, 5, 3, 4, 5]
+
+listCheck(numberList)
+# Jamaree Jackson
+# 3/7/19
+
+# Checks if the sum of two user inputs are equal to, greater than, or less
+
+number1 + int(input("Enter in number 1:"))
+number2 + int(input("Enter in number 2:"))
+
+def numbercheck(n1, n2):
+    if n1 + n2 == 10:
+        print("Equal")
+    elif nl + n2 > 10:
+        print("Greater Than")
+    else:
+        print("less Then")
+
+#numbercheck(number1, number2)
+print(numbercheck(number1, number))
+# Checks if two user inputs are equal
+
+
+number1 = int(input("Enter in number 1: "))
+number2 = int(input("Enter in number 2: "))
+def numbercheck(nl, n2):
+    if nl == n2:
+        print("true")
+        return True
+    else:
+        print("false")
+        return False
+
+numbercheck(number1, number2)
+print(numbercheck(number1, number2))
---
+++ module 9.py
@@ -0,0 +1,31 @@
+#Dante Howard
+#03/14/19
+
+#This is the code for a while loop
+
+while True:
+    print("infinate")
+    #03/14/19
+
+while counter < 50:
+    if counter % 10 == 0
+    L.append(counter)
+    counter + = 1
+
+print(L)
+L = []
+counter = 1
+
+while counter < 35:
+    L.append(counter)
+    counter + = 1
+
+print(L)
+L = []
+counter = 0
+
+while counter < 11:
+    L.append(counter)
+    counter + = 1
+
+print(L)
