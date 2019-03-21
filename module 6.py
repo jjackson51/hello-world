bcbb548eea71844130f28253f36ac5ddec408eec
jjackson51 <48562392+jjackson51@users.noreply.github.com>
2019-03-21 12:38:34 -0500

    Add files via upload

---
+++ module 6.py
@@ -0,0 +1,20 @@
+import random
+
+print (random.randrange(0,100,2)+1)
+import math
+
+radians (float(input("how many radians do you have"))
+print (radians*180/math.pi)
+         import random
+
+for x in range(10):
+    print (random.randrange(25,35))
+         import math
+
+print (22/7)
+print (math.pi)
+         import random
+
+days = ["Sunday", "Monday", "Tuesday", "Thurday", "Friday", "Saturday"]
+
+print (random.choice(day))
